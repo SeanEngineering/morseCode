f9d12393e5d2dbc3cd736d942236c436a4323e93
Sean <sean@Seans-MBP.modem>
2022-10-21 08:51:36 +1100

    fixed multiple new lines bug

--- conversionFunctions.js
+++ conversionFunctions.js
@@ -1,80 +1,90 @@
 export const morseLibrary = {
-  a: ".-",
-  b: "-...",
-  c: "-.-.",
-  d: "-..",
-  e: ".",
-  f: "..-.",
-  g: "--.",
-  h: "....",
-  i: "..",
-  j: ".---",
-  k: "-.-",
-  l: ".-..",
-  m: "--",
-  n: "-.",
-  o: "---",
-  p: ".--.",
-  q: "--.-",
-  r: ".-.",
-  s: "...",
-  t: "-",
-  u: "..-",
-  v: "...-",
-  w: ".--",
-  x: "-..-",
-  y: "-.--",
-  z: "--..",
-  1: ".----",
-  2: "..---",
-  3: "...--",
-  4: "....-",
-  5: ".....",
-  6: "-....",
-  7: "--...",
-  8: "---..",
-  9: "----.",
-  0: "-----",
-  ",": "--..--",
-  ".": ".-.-.-",
-  "?": "..--..",
-  "!": "-.-.--",
-  '"': ".-..-.",
-  ":": "---...",
-  "'": ".----.",
-  "-": "-...-",
-  "(": "-.--.",
-  ")": "-.--.-",
+  a: '.-',
+  b: '-...',
+  c: '-.-.',
+  d: '-..',
+  e: '.',
+  f: '..-.',
+  g: '--.',
+  h: '....',
+  i: '..',
+  j: '.---',
+  k: '-.-',
+  l: '.-..',
+  m: '--',
+  n: '-.',
+  o: '---',
+  p: '.--.',
+  q: '--.-',
+  r: '.-.',
+  s: '...',
+  t: '-',
+  u: '..-',
+  v: '...-',
+  w: '.--',
+  x: '-..-',
+  y: '-.--',
+  z: '--..',
+  1: '.----',
+  2: '..---',
+  3: '...--',
+  4: '....-',
+  5: '.....',
+  6: '-....',
+  7: '--...',
+  8: '---..',
+  9: '----.',
+  0: '-----',
+  ',': '--..--',
+  '.': '.-.-.-',
+  '?': '..--..',
+  '!': '-.-.--',
+  '"': '.-..-.',
+  ':': '---...',
+  "'": '.----.',
+  '-': '-...-',
+  '(': '-.--.',
+  ')': '-.--.-',
 };
 
 export const convertToEng = (string) => {
-  let morseArray = string.split(" ");
+  let morseArray = string.split(' ');
   let englishArray = [];
 
   for (let item of morseArray) {
     //Handling new lines
-    if (item.includes("\n")) {
-      let temp = item.split("\n");
+    if (item.includes('\n')) {
+      let temp = item.split('\n');
       console.log(temp);
-      englishArray.push(" ");
+      englishArray.push(' ');
       for (let properties of temp) {
-        console.log(properties)
-        if (properties == "|" || properties == "/"){
-          englishArray.push(" ");
-        } else if (Object.keys(morseLibrary).find((key) => morseLibrary[key] === properties) == undefined){
+        console.log(properties);
+        if (properties == '|' || properties == '/') {
+          englishArray.push(' ');
+        } else if (properties == '') {
+          continue;
+        } else if (
+          Object.keys(morseLibrary).find(
+            (key) => morseLibrary[key] === properties
+          ) == undefined
+        ) {
           return `(${properties} is not in defined in this library)`;
         } else {
           englishArray.push(
-            Object.keys(morseLibrary).find((key) => morseLibrary[key] === properties)
+            Object.keys(morseLibrary).find(
+              (key) => morseLibrary[key] === properties
+            )
           );
         }
-        
       }
       //Standard behaviour
     } else {
-      if (item == "|" || item == "/") {
-        englishArray.push(" ");
-      } else if (Object.keys(morseLibrary).find((key) => morseLibrary[key] === item) == undefined){
+      if (item == '|' || item == '/') {
+        englishArray.push(' ');
+      } else if (
+        Object.keys(morseLibrary).find((key) => morseLibrary[key] === item) ==
+        undefined
+      ) {
         return `(${item} is not in morse library)`;
       } else {
         englishArray.push(
@@ -84,20 +94,20 @@ export const convertToEng = (string) => {
     }
   }
 
-  return englishArray.join("");
+  return englishArray.join('');
 };
 
 export const convertToMorse = (string) => {
-  if (typeof string == "number") {
+  if (typeof string == 'number') {
     string = string.toString();
   }
-  let array = string.toLowerCase().split("");
+  let array = string.toLowerCase().split('');
   let morseArray = [];
   for (let i = 0; i < array.length; i++) {
-    if (array[i] == " ") {
-      morseArray.push("| ");
-    } else if (array[i] == "\n") {
-      morseArray.push("\n");
+    if (array[i] == ' ') {
+      morseArray.push('| ');
+    } else if (array[i] == '\n') {
+      morseArray.push('\n');
     } else if (morseLibrary[array[i]] == null) {
       return `(${array[i]} is not defined in this library)`;
     } else {
@@ -105,5 +115,5 @@ export const convertToMorse = (string) => {
     }
   }
 
-  return morseArray.join("").trim();
+  return morseArray.join('').trim();
 };
