2d415ed1dc2db591bffcc0cb13b938f799ae18b5
Sean <sean@Seans-MBP.modem>
2022-10-21 09:35:40 +1100

    moved around files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {morseLibrary, convertToEng, convertToMorse} from "./conversionFunctions.js"
+import { convertToEng, convertToMorse } from './conversionFunctions.js';
 const englishBox = document.getElementById('english');
 const morseBox = document.getElementById('morse');
 const morseCopy = document.getElementById('morseCopy');
@@ -7,21 +7,25 @@ const englishCopy = document.getElementById('englishCopy');
 morseCopy.addEventListener('click', () => {
   navigator.clipboard.writeText(morseBox.value);
   morseCopy.style.opacity = 0.5;
-  setTimeout(()=>{morseCopy.style.opacity = 1;}, 100);
-})
+  setTimeout(() => {
+    morseCopy.style.opacity = 1;
+  }, 100);
+});
 
 englishCopy.addEventListener('click', () => {
   navigator.clipboard.writeText(englishBox.value);
   englishCopy.style.opacity = 0.5;
-  setTimeout(()=>{englishCopy.style.opacity = 1;}, 100);
-})
-englishBox.addEventListener('keypress', function enterPress(e){
-  if (e.which==13){
+  setTimeout(() => {
+    englishCopy.style.opacity = 1;
+  }, 100);
+});
+englishBox.addEventListener('keypress', function enterPress(e) {
+  if (e.which == 13) {
     morseBox.value = convertToMorse(englishBox.value);
   }
-})
-morseBox.addEventListener('keypress', function enterPress(e){
-  if (e.which==13){
+});
+morseBox.addEventListener('keypress', function enterPress(e) {
+  if (e.which == 13) {
     englishBox.value = convertToEng(morseBox.value);
   }
-})
+});
--- script.test.js
+++ script.test.js
@@ -1,66 +1,70 @@
-import {
-  morseLibrary,
-  convertToEng,
-  convertToMorse,
-} from './conversionFunctions.js';
-
+import { convertToEng, convertToMorse } from './conversionFunctions.js';
+////////////////////////////////////////////////////////////////
 describe('Converts to Morse', () => {
+  const testA = [
+    ['hello world', '.... . .-.. .-.. --- | .-- --- .-. .-.. -..'],
+    ['HELLO WORLD', '.... . .-.. .-.. --- | .-- --- .-. .-.. -..'],
+    [
+      'The number is 12345',
+      '- .... . | -. ..- -- -... . .-. | .. ... | .---- ..--- ...-- ....- .....',
+    ],
+  ];
+
   it('handles lower case', () => {
-    expect(convertToMorse('hello world')).toBe(
-      '.... . .-.. .-.. --- | .-- --- .-. .-.. -..'
-    );
+    expect(convertToMorse(testA[0][0])).toBe(testA[0][1]);
   });
   it('handles upper case', () => {
-    expect(convertToMorse('HELLO WORLD')).toBe(
-      '.... . .-.. .-.. --- | .-- --- .-. .-.. -..'
-    );
+    expect(convertToMorse(testA[1][0])).toBe(testA[1][1]);
   });
   it('Accounts for numbers', () => {
-    expect(convertToMorse('The number is 12345')).toBe(
-      '- .... . | -. ..- -- -... . .-. | .. ... | .---- ..--- ...-- ....- .....'
-    );
+    expect(convertToMorse(testA[2][0])).toBe(testA[2][1]);
   });
 });
+////////////////////////////////////////////////////////////////
 describe('Converts to English', () => {
+  const testB = [
+    ['.... . .-.. .-.. --- | .-- --- .-. .-.. -..', 'hello world'],
+    ['.... . .-.. .-.. --- / .-- --- .-. .-.. -..', 'hello world'],
+    [
+      '- .... . | -. ..- -- -... . .-. | .. ... | .---- ..--- ...-- ....- .....',
+      'the number is 12345',
+    ],
+  ];
+
   it('converts hello world', () => {
-    expect(convertToEng('.... . .-.. .-.. --- | .-- --- .-. .-.. -..')).toBe(
-      'hello world'
-    );
+    expect(convertToEng(testB[0][0])).toBe(testB[0][1]);
   });
   it('handles slash notation', () => {
-    expect(convertToEng('.... . .-.. .-.. --- / .-- --- .-. .-.. -..')).toBe(
-      'hello world'
-    );
+    expect(convertToEng(testB[1][0])).toBe(testB[1][1]);
   });
   it('Accounts for numbers', () => {
-    expect(
-      convertToEng(
-        '- .... . | -. ..- -- -... . .-. | .. ... | .---- ..--- ...-- ....- .....'
-      )
-    ).toBe('the number is 12345');
+    expect(convertToEng(testB[2][0])).toBe(testB[2][1]);
   });
 });
-
+////////////////////////////////////////////////////////////////
 describe('Handles unexpected chars', () => {
+  const testC = [
+    ['hello `world', '(hello is not defined in this morse library)'],
+    ['*', '(* is not defined in this library)'],
+  ];
+
   it("returns the first component that's not in the library to eng", () => {
-    expect(convertToEng('hello `world')).toBe(
-      '(hello is not defined in this morse library)'
-    );
+    expect(convertToEng(testC[0][0])).toBe(testC[0][1]);
   });
   it("returns the first component that's not in the library", () => {
-    expect(convertToMorse('*')).toBe('(* is not defined in this library)');
+    expect(convertToMorse(testC[1][0])).toBe(testC[1][1]);
   });
 });
-
+////////////////////////////////////////////////////////////////
 describe('Handles new lines', () => {
+  const testD = [
+    ['.... . .-.. .-.. ---\n.-- --- .-. .-.. -..', 'hello world'],
+    ['.... . .-.. .-.. ---\n\n\n\n.-- --- .-. .-.. -..', 'hello    world'],
+  ];
   it('handles a new line', () => {
-    expect(convertToEng('.... . .-.. .-.. ---\n.-- --- .-. .-.. -..')).toBe(
-      'hello world'
-    );
+    expect(convertToEng(testD[0][0])).toBe(testD[0][1]);
   });
   it('converts multiple new lines to spaces', () => {
-    expect(
-      convertToEng('.... . .-.. .-.. ---\n\n\n\n.-- --- .-. .-.. -..')
-    ).toBe('hello    world');
+    expect(convertToEng(testD[1][0])).toBe(testD[1][1]);
   });
 });
